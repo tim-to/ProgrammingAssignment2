fccbd806f42c6b8dd69be981d42b0ece01d19fde
Tim To <tim.to.van@gmail.com>
2015-02-17 00:47:51 -0800

    Added comments to R script for overall comment and the two functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
+## For expensive computations, caching is a great technique to speed things up. 
+## These function will cache the inverse of a matrix then return a cached copy
+## to improve performance of that matrix.
+## Both functions assume the input matrix is a square matrix that can be 
+## inversed. 
 
-## Write a short comment describing this function
+## This function will cache the inverse of a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This function will compute the inverse of a matrix if it isn't already 
+## cached. If it is cached, it will return the cached version.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
